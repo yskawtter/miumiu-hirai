a8a81d6a3a6c3ab70677177c29c5a1d59312e853
Yuri Souza <yskawtter77@gmail.com>
2023-11-07 22:45:02 -0300

    screen responsive and fix error in grid

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".header-nav");
 
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+const navlink = document.querySelector(".header-nav__links");
+navlink.addEventListener("click", (e) => {
+  let target = e.target;
+  if (target.className !== "link") return;
+  nav.classList.toggle("active");
+});
